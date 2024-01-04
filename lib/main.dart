ee0a459259f1772b3abb194267272f33f7793a0a
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2024-01-04 14:44:32 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,46 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/login.dart';
 
 void main() {
   runApp(const MyApp());
 }
+  /*
+  AwesomeNotifications().initialize(
+      null,
+      [
+        NotificationChannel(
+            channelKey: 'basica_Channel',
+            channelName: "Basic Notifications",
+            channelDescription: "Notification channel for basic tests")
+      ],
+    debug: true,
+  );
+  */
+/*
+void requestNotificationPermission() async {
+  await AwesomeNotifications().requestPermissionToSendNotifications();
+  bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowed) {
 
+  }
+
+}
+
+void scheduleNotification() async {
+  await AwesomeNotifications().createNotification(
+    content: NotificationContent(
+      id: 0,
+      channelKey: 'basic_channel',
+      title: 'Scheduled Notification',
+      body: 'This notification was scheduled at a specific time.',
+    ),
+    schedule: NotificationCalendar(
+      second: DateTime.now().second + 10, // Schedule the notification 10 seconds from now
+    ),
+  );
+}
+*/
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
